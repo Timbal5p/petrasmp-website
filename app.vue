c2d4988bb0ac9830ff08772d407024ef77478010
timbal5p <108987243+Timbal5p@users.noreply.github.com>
2025-08-06 18:18:01 +0200

    added text

--- app.vue
+++ app.vue
@@ -20,15 +20,17 @@
         <li>and more...</li>
       </ul>
       <br/>
+      <h2 class="display text-center text-clip font-inter text-5xl font-bold">What is petraSMP?</h2>
+      <p class="display text-center text-clip font-inter font-bold text-xl">petraSMP is a Survival Multiplayer Minecraft Server where people play in survival worlds together. There are also a few plugins to change your experience a bit. For example, ore veins will be destroyed instantly when one block is mined. Or also Voice Chat (mod required) to chat via voice. Also, the server has daily backups so your builds are secure. The list of plugins is above this section, you can look them up if you want. Other than plugins, there are also some settings such as PvP on. And if you want, you can become are supporter to keep this Server online.</p>
       <h2 class="display text-center font-inter text-5xl font-bold bg-clip-text bg-linear-to-r from-amber-600 to-yellow-200 text-transparent">Become Supporter</h2>
       <ul class="display text-center text-clip font-inter list-disc list-inside text-xl bg-clip-text bg-linear-to-r from-amber-600 to-yellow-200 text-transparent">
         <li>try features out before anyone else</li>
         <li>get access to exclusive content (like behind the scenes)</li>
         <li>become part of the private community</li>
       </ul>
-      <h2 class="display text-center font-inter text-4xl font-bold bg-clip-text bg-linear-to-r from-amber-600 to-yellow-200 text-transparent">become member now at:</h2>
-      <a href="https://ko-fi.com/petragamingantivirus" class="display text-center font-inter text-4xl font-bold bg-clip-text bg-linear-to-r from-amber-600 to-yellow-200"><h2>Kofi</h2></a>
-      <a href="https://patreon.com/petragaming" class="display text-center font-inter text-4xl font-bold bg-clip-text bg-linear-to-r from-amber-600 to-yellow-200"><h2>Patreon</h2></a>
+      <h2 class="display text-center font-inter text-4xl font-bold bg-clip-text bg-linear-to-r from-amber-600 to-yellow-200 text-transparent">donate and become member now at:</h2>
+      <a href="https://ko-fi.com/petragamingantivirus" class="display underline text-center font-inter text-4xl font-bold bg-clip-text bg-linear-to-r from-amber-600 to-yellow-200"><h2>Kofi</h2></a>
+      <a href="https://patreon.com/petragaming" class="display underline text-center font-inter text-4xl font-bold bg-clip-text bg-linear-to-r from-amber-600 to-yellow-200"><h2>Patreon</h2></a>
       <br/>
       <h2 class="display text-center text-clip font-inter text-4xl lg:text-6xl md:text-5xl sm:text-4xl font-bold bg-linear-to-l from-lime-200 to-indigo-950 size-fit m-auto p-10 tracking-normal duration-500 rounded-xl hover:p-13 hover:tracking-widest hover:rounded-none">What are you waiting for?</h2>
       <h3 class="display text-center font-inter text-clip text-3xl">play.petragamingmc.com</h3>
